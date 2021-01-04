5df42cefb68355781c44cbe2d41dc33912c36a0a
Marcos  Yamashita <marcospauloyamashita@gmail.com>
2021-01-04 02:51:25 -0300

    Ajustando Front Bootstrap

--- painel.php
+++ painel.php
@@ -12,23 +12,24 @@ include('verifica_login.php');
 ?>
 
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Login - DIARIO DE BORDO</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <title>Diário De Bordo</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="css/style.css" type="text/css" rel="stylesheet">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </head>
 
 <body>
 
-<h2>Olá,  <?php echo $_SESSION['usuario'];?></h2><h2><a href="logout.php">Sair</a></h2>;
+<h2>Olá,  <?php echo $_SESSION['usuario'];?></h2><h2><a href="logout.php">Sair</a></h2>
 
 <nav class="mb-1 navbar navbar-expand-lg navbar-dark bg-info">
     <a class="navbar-brand" href="#">Diário De Bordo</a>
@@ -53,17 +54,47 @@ include('verifica_login.php');
     <section id="tabs">
         <div class="container">
             <div class="row">
-                <div class="col-xs-12 ">
+                <div class="col-sm-12">
                     <nav>
                         <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
                             <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Criar Registro</a>
-                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Consultar Registro</a>
-                            <a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="false">Relatorios</a>
+                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="true">Consultar Registro</a>
+                            <a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="true">Relatorios</a>
                         </div>
                     </nav>
                     <div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
                         <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                            Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+                            <form>
+                                <div class="form-group form-inline row">
+                                    <label for="example-date-input" class="col-2 col-form-label">Inicio</label>
+                                    <div class="col-8">
+                                        <input class="form-control" type="date" value="" id="dateInicio">
+                                        <input class="form-control" type="time" value="" id="timeInicio">
+                                    </div>
+                                </div>
+                                <div class="form-group form-inline row">
+                                    <label for="example-date-input" class="col-2 col-form-label">Fim</label>
+                                    <div class="col-8">
+                                        <input class="form-control" type="date" value="" id="dateFim">
+                                        <input class="form-control" type="time" value="" id="timeFim">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="example-text-input" class="col-2 col-form-label">Titulo:</label>
+                                    <div class="col-10">
+                                        <input class="form-control" type="text" value="" id="titulo">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="example-text-input" class="col-2 col-form-label">Atividade:</label>
+                                    <div class="col-10">
+                                        <input class="form-control" type="text" value="" id="atividade">
+                                    </div>
+                                </div>
+
+                                <input type="button" value="Gravar"><input type="reset" value="Resetar">
+
+                            </form>
                         </div>
                         <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
                             Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
